54c0d85c29bd50f54f34409f4a01bc8a3cfc15e6
Piotr Jakubowski <piotr.jakubowski@gmail.com>
2026-06-19 14:48:05 +0200

    Change applicationId to com.indiana.zwllokator to avoid package conflict on installation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.zwl"
+        applicationId = "com.indiana.zwllokator"
         minSdk = 26
         targetSdk = 34
         versionCode = 1
