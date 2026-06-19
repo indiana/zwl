3b6e3c6b765df998a21971e86bed14ea4f425807
Piotr Jakubowski <piotr.jakubowski@gmail.com>
2026-06-19 12:56:58 +0200

    Fix Compose testing dependency notation in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -93,7 +93,7 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.compose.ui.test-junit4)
+    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
-    debugImplementation(libs.androidx.compose.ui.test-manifest)
+    debugImplementation(libs.androidx.compose.ui.test.manifest)
 }
