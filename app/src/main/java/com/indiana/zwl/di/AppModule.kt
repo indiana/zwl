038fdc4fe972e0ece9c40a55778dff79993dbf49
Piotr Jakubowski <piotr.jakubowski@gmail.com>
2026-06-20 10:54:51 +0200

    Fix Hilt AppModule bindings to return interface types

--- app/src/main/java/com/indiana/zwl/di/AppModule.kt
+++ app/src/main/java/com/indiana/zwl/di/AppModule.kt
@@ -3,6 +3,8 @@ package com.indiana.zwl.di
 import android.content.Context
 import com.indiana.zwl.data.repository.CompassRepositoryImpl
 import com.indiana.zwl.data.repository.LocationRepositoryImpl
+import com.indiana.zwl.domain.CompassRepository
+import com.indiana.zwl.domain.LocationRepository
 import com.indiana.zwl.domain.SpatialEngine
 import dagger.Module
 import dagger.Provides
@@ -17,13 +19,13 @@ object AppModule {
 
     @Provides
     @Singleton
-    fun provideLocationRepository(@ApplicationContext context: Context): LocationRepositoryImpl {
+    fun provideLocationRepository(@ApplicationContext context: Context): LocationRepository {
         return LocationRepositoryImpl(context)
     }
 
     @Provides
     @Singleton
-    fun provideCompassRepository(@ApplicationContext context: Context): CompassRepositoryImpl {
+    fun provideCompassRepository(@ApplicationContext context: Context): CompassRepository {
         return CompassRepositoryImpl(context)
     }
 
