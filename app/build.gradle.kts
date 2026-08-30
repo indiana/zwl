9bf23ad41f34ec2a35a8cdbdc85508c4d7604ae4
Piotr Jakubowski <piotr.jakubowski@gmail.com>
2026-08-30 22:23:05 +0200

    Bump Android version to 1.12 (versionCode 13)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.indiana.zwl"
         minSdk = 26
         targetSdk = 36
-        versionCode = 12
-        versionName = "1.11"
+        versionCode = 13
+        versionName = "1.12"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
