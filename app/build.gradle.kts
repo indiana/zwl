1337834c4471fa3fbcb328ee98078fd868020f0e
Piotr Jakubowski <piotr.jakubowski@gmail.com>
2026-09-01 23:42:20 +0200

    Bump Android versionCode 14 / versionName 1.13 for Play Store release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.indiana.zwl"
         minSdk = 26
         targetSdk = 36
-        versionCode = 13
-        versionName = "1.12"
+        versionCode = 14
+        versionName = "1.13"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
