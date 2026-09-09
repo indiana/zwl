8c1046cc8d404e140eff41fbeb9dbfec06e364ab
Piotr Jakubowski <piotr.jakubowski@gmail.com>
2026-09-09 23:49:10 +0200

    Bump Android versionCode 19 / versionName 1.18 (sync with main to avoid merge conflict)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.indiana.zwl"
         minSdk = 26
         targetSdk = 36
-        versionCode = 18
-        versionName = "1.17"
+        versionCode = 19
+        versionName = "1.18"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
