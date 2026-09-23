cf970a089e6a8ea332229e3c59edee76681ad173
Piotr Jakubowski <piotr.jakubowski@gmail.com>
2026-09-23 15:57:06 +0200

    Bump versions (Android 21/1.20, iOS 62/1.4)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.indiana.zwl"
         minSdk = 26
         targetSdk = 36
-        versionCode = 20
-        versionName = "1.19"
+        versionCode = 21
+        versionName = "1.20"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
