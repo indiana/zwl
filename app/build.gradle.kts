be1bf1bf8c4c83804adf69f422df9b1affc1d77c
Piotr Jakubowski <piotr.jakubowski@gmail.com>
2026-09-24 13:34:55 +0200

    Bump Android versionCode 22 / versionName 1.21

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.indiana.zwl"
         minSdk = 26
         targetSdk = 36
-        versionCode = 21
-        versionName = "1.20"
+        versionCode = 22
+        versionName = "1.21"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
